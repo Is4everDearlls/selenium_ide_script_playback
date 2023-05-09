15df519e8c5157871b62096f80b75f860249a8e1
廖加权 <liaojiaquan@zj.tech>
2023-05-09 17:51:19 +0800

    operable.py finish

--- selenium_ide_script_playback_tools/operable.py
+++ selenium_ide_script_playback_tools/operable.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Union
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -15,21 +16,27 @@ class BaseWebOperation(metaclass=abc.ABCMeta):
     def __init__(self, driver=None):
         self.driver = driver
 
-    def window_handles(self, key='window_handles', single=True):
-        if single:
-            self.GLOBAL_WINDOW_HANDLES[key] = self.driver.current_window_handle
-        else:
-            self.GLOBAL_WINDOW_HANDLES[key] = self.driver.window_handles
-        return self.GLOBAL_WINDOW_HANDLES.get(key)
+    def window_handles(self, key: str = 'window_handles'):
+        window_handles = self.driver.window_handles
+        if key:
+            self.GLOBAL_WINDOW_HANDLES[key] = window_handles
+        return window_handles
 
-    def wait_for_new_window(self, fn, key, timeout, *args, **kwargs):
+    def current_window_handle(self, key: str = None):
+        current_window_handle = self.driver.current_window_handle
+        if key:
+            self.GLOBAL_WINDOW_HANDLES[key] = current_window_handle
+        return current_window_handle
+
+    def wait_new_window_handle(self, key: str = None, timeout: int = 10):
         self.window_handles('window_handles')
-        fn(*args, **kwargs)
         window_handles = self.GLOBAL_WINDOW_HANDLES["window_handles"]
         WebDriverWait(self.driver, timeout).until(expected.new_window_is_opened(window_handles))
         new_window_handles = self.driver.window_handles
         new_window_handle = set(new_window_handles).difference(set(window_handles)).pop()
-        self.GLOBAL_WINDOW_HANDLES[key] = new_window_handle
+        if key:
+            self.GLOBAL_WINDOW_HANDLES[key] = new_window_handle
+        return new_window_handle
 
     def switch_to_window(self, handles):
         if handles in self.GLOBAL_WINDOW_HANDLES.keys():
@@ -40,7 +47,7 @@ class BaseWebOperation(metaclass=abc.ABCMeta):
     def close(self):
         self.driver.close()
 
-    def wait_for_find_element(self, locator, timeout=None, message=None, ec=None):
+    def find_element(self, locator, timeout=None, message=None, ec=None):
         if not ec:
             ec = BaseWebOperation.DEFAULT_WAIT_EXPECTED
         if not timeout:
@@ -48,25 +55,24 @@ class BaseWebOperation(metaclass=abc.ABCMeta):
         return WebDriverWait(self.driver, timeout).until(ec(locator), message)
 
     def click(self, locator, timeout=None, message=None, ec=None):
-        self.wait_for_find_element(locator, timeout, message, ec).click()
+        self.find_element(locator, timeout, message, ec).click()
 
     def maximize_window(self):
         self.driver.maximize_window()
 
     def send_keys(self, locator, value, timeout=None, message=None, ec=None):
-        self.wait_for_find_element(locator, timeout, message, ec).send_keys(value)
+        self.find_element(locator, timeout, message, ec).send_keys(value)
 
     def get(self, url):
         self.driver.get(url)
 
     def move_to_element(self, locator, timeout=None, message=None, ec=None):
-        element = self.wait_for_find_element(locator, timeout, message, ec)
-        self.action_chains().move_to_element(element).perform()
-        element = self.wait_for_find_element((By.CSS_SELECTOR, "body"), timeout)
-        self.action_chains().move_to_element_with_offset(element, 0, 0).perform()
+        self.action_chains().move_to_element(self.find_element(locator, timeout, message, ec)).perform()
+        body = self.find_element((By.CSS_SELECTOR, "body"), timeout)
+        self.action_chains().move_to_element_with_offset(body, 0, 0).perform()
 
     def double_click(self, locator, timeout=None, message=None, ec=None):
-        web_element = self.wait_for_find_element(locator, timeout, message, ec)
+        web_element = self.find_element(locator, timeout, message, ec)
         self.action_chains().double_click(web_element).perform()
 
     def action_chains(self):
