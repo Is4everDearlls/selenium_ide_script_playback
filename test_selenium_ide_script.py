7c863a26c2110c8e7b1e27228e78641791f936ab
廖加权 <liaojiaquan@zj.tech>
2023-05-10 13:16:56 +0800

    operable.py finish

--- test_selenium_ide_script.py
+++ test_selenium_ide_script.py
@@ -1,3 +1,15 @@
+import os
+import sys
+
+import pytest
+
+
 def test_selenium_ide_script(result):
     result.write()
     assert result.result
+
+
+if __name__ == '__main__':
+    pytest.main(['-s', '-v', '--alluredir', 'result', '--clean-alluredir'])
+    os.system('allure generate --clean result --output result/html')
+    sys.exit(0)
